1a267db9933b223e3b0ce90f26ba8ddde431e6b8
alisaidgunes <alisaidgunes1@gmail.com>
2022-12-18 22:48:50 +0300

    UPDATE

---
+++ server/StoneGame/src/main/java/com/example/stonegame/users/entities/User.java
@@ -0,0 +1,25 @@
+package com.example.stonegame.users.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+}
